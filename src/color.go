30840f96e012b753c4183f3d47253e94546c8da7
txmfac <tom.facon@epitech.eu>
2024-06-26 16:10:50 +0200

    ✨ feat: add colors const in dedicated file

---
+++ src/color.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	ColorBlack		= "\u001b[30m"
+	ColorRed          = "\u001b[31m"
+	ColorGreen        = "\u001b[32m"
+	ColorYellow       = "\u001b[33m"
+	ColorBlue         = "\u001b[34m"
+	ColorReset        = "\u001b[0m"
+)
