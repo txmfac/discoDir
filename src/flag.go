427c9ab62c3f0cad57add4c24ecb50b39e21b06c
txmfac <tom.facon@epitech.eu>
2024-06-26 15:32:01 +0200

    ✨ feat(Flag): create Flag.go and implement flagInit function.

---
+++ src/flag.go
@@ -0,0 +1,16 @@
+package main
+
+import "flag"
+
+type flagStruct struct {
+	list string
+	target string
+	suf string
+}
+
+func flagInit (test * flagStruct) {
+	flag.StringVar(&test.list, "l", "", "file with all the subdir to scan.")
+	flag.StringVar(&test.target, "u", "0.0.0.0", "url of the target.")
+	flag.StringVar(&test.target, "s", "", "suffixes to add at the end of the url.")
+	flag.Parse()
+}
